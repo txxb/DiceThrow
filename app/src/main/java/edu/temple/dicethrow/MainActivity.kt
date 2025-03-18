e3b0468f9df4dd12c0658137855e149155e7dd2b
Maab Taha <tmaab13@gmail.com>
2025-03-18 11:40:25 -0400

    roll button wired to event

--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,14 +1,23 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+            (supportFragmentManager
+                .findFragmentById(R.id.fragmentContainerView) as DieFragment)
+                .throwDie()
+        }
     }
 }
